288a279d9ffa08854f554312b6bbd49184bc2802
InaamR <inaam.rach@gmail.com>
2021-08-09 03:18:43 +0200

    update

--- pages/mentions.php
+++ pages/mentions.php
@@ -97,7 +97,7 @@
 									<b>Directeur de la publication</b> : Hedi JMEL<br><br>
 									<b>Co-fondateur</b> : Zied GROMBALI<br><br>
 
-									<b>Numéros d'appel</b> (standard du siège social) : Téléphone : +21658554255  du lundi au vendredi de 9h00 à 12h30 et de 13h45 à 18h 
+									<b>Numéros d'appel</b> (standard du siège social) : Téléphone : +216 58 554 255  du lundi au vendredi de 9h00 à 12h30 et de 13h45 à 18h 
 								</div>
 
 							</div>
@@ -113,7 +113,7 @@
 									<div class="about-text">
                                 	<b>Développeurs</b> : Mohamed Inaam RACHDI et Malek MENCHAOUI<br><br>
 
-									<b>Désigneur intégrateur</b> : Zied JLASSI<br><br>
+									<b>Designeur intégrateur</b> : Zied JLASSI<br><br>
 
 									<b>Graphiste</b> : Negi CHAIEB<br><br>
 								</div>
